4ae5aff328f81a6265fcc7570543b58e01cf7152
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-13 13:33:22 -0500

    Add project files.

---
+++ ClassesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassesDemo;
+
+Car jeep = new Car();
+
+jeep.make = "Jeep";
+jeep.year = 2016;
+jeep.model = "Cherokee";
+
+
+Console.WriteLine($"Our first car is a {jeep.make} {jeep.model} built in year {jeep.year}");
+
+
